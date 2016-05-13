3a4b6673cba622ab0195e9908fe97bf7e6415927
De Wet <dewet@blomerus.org>
2016-05-13 13:36:03 +0200

    atom

--- desktop-only.sh
+++ desktop-only.sh
@@ -16,13 +16,14 @@ if [ ! -f "/etc/apt/sources.list.d/google.list" ]; then
 fi
 
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-2
+sudo add-apt-repository -y ppa:webupd8team/atom
 sudo apt-get update
 
 # This is not needed for Linux Mint
 #sudo apt-get -y ubuntu-restricted-extras vlc libreoffice-base gimp
 
 echo "\n apt-get install stuff \n"
-sudo apt-get -y install google-chrome-stable sublime-text skype dropbox python-gpgme audacity klavaro git-gui virtualbox-5.0 aria2 autokey-gtk
+sudo apt-get -y install google-chrome-stable sublime-text skype dropbox python-gpgme audacity klavaro git-gui virtualbox-5.0 aria2 autokey-gtk atom
 
 sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
